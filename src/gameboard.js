ed9ef52bd0b1d3f3aba3f16c9dfcc74fbcd3a4c2
Jordan Ulmer <julmer526@gmail.com>
2022-08-23 17:55:00 +0100

    wireframed gameboard factory

---
+++ src/gameboard.js
@@ -0,0 +1,24 @@
+// factory function that places ships at coordinates and track attacks
+
+import { Ship } from "./shipFactory";
+
+const Gameboard = () => {
+  const placeShip = () => {
+    // function that places a ship, by calling the Ship factory function
+  };
+
+  const recieveAttack = (x, y) => {
+    // takes pair of coordinates, determines if the attack hit a ship, then sends the hit function
+    // to the correct ship
+  };
+
+  const missedAttacks = () => {
+    // an ?array? of attacks that are missed and can be displayed properly
+  }
+
+  const allShipsSunk = () => {
+    // function that checks if all ships are sunk or not, and therefore end the game when true
+  }
+};
+
+export {Gameboard}
